6e8d8845fa46ad9a8468f33fcb73ecd31d382497
Nosh-H <haskell.nh@gmail.com>
2026-05-08 23:43:28 -0400

    add new dist

---
+++ dist/assets/ProjectsView-Dn_yY-Yw.js
@@ -0,0 +1,1 @@
+import{d as s,c as o,a as t,b as a,u as r,p as c,P as n,o as p,_ as l}from"./index-Cw-2WI8k.js";const _=s({__name:"ProjectsView",setup(u){return(d,e)=>(p(),o("main",null,[e[0]||(e[0]=t("section",{class:"projects-hero"},null,-1)),a(n,{projects:r(c)},null,8,["projects"])]))}}),j=l(_,[["__scopeId","data-v-2d423199"]]);export{j as default};
